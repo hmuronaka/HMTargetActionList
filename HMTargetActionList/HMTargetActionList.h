d0aba5b25c6244bf9d0b3f43aa67620877af0062
hmuronaka <capriccio27@gmail.com>
2015-09-04 20:03:34 +0900

    for carthage

---
+++ HMTargetActionList/HMTargetActionList.h
@@ -0,0 +1,20 @@
+//
+//  HMTargetActionList.h
+//  HMTargetActionList
+//
+//  Created by Muronaka Hiroaki on 2015/09/04.
+//  Copyright (c) 2015年 Muronaka Hiroaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HMTargetActionList.
+FOUNDATION_EXPORT double HMTargetActionListVersionNumber;
+
+//! Project version string for HMTargetActionList.
+FOUNDATION_EXPORT const unsigned char HMTargetActionListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMTargetActionList/PublicHeader.h>
+#import <HMTargetActionList/HMTargetActionList.h>
+#import <HMTargetActionList/HMWeakTargetAction.h>
+
